9ec9166768319b24b7476eb5da59af1572ff19f1
ER-RADA Oumaima <erradaoumaima@gmail.com>
2024-11-08 17:31:45 +0100

    fixer le probleme de merge

--- src/main/java/com/pigeonskyrace/PigeonSkyRaceApplication.java
+++ src/main/java/com/pigeonskyrace/PigeonSkyRaceApplication.java
@@ -9,27 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.bson.types.ObjectId;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.pigeonskyrace")
 public class PigeonSkyRaceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PigeonSkyRaceApplication.class, args);
 	}
-
-//	@Bean
-//	CommandLineRunner runner(UserRepository userRepository) {
-//		return args -> {
-//			System.out.println("Tentative d'insertion de l'utilisateur...");
-//
-//			User user = new User()
-//					.setName("test")
-//					.setEmail("test1@gmail.com")
-//					.setPassword("password1234")
-//					.setRole(Role.ELEVEUR);
-//
-//			userRepository.save(user);
-//
-//			System.out.println("Utilisateur ajouté à la base de données : " + user);
-//		};
-//	}
 }
